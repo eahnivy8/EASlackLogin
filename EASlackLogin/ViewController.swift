8d7846644bc12322a84f4994a81b2784b49724b0
Eddie <eahnivy8@gmail.com>
2020-01-06 11:55:45 +0900

    Button

--- EASlackLogin/ViewController.swift
+++ EASlackLogin/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var urlField: UITextField!
+    
+    @IBOutlet weak var placeholderLabel: UILabel!
+    @IBOutlet weak var placeholderLeadingConstraints: NSLayoutConstraint!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
